9bd4a067f4bc0b4013a5629b77d2d785b45e89bd
Vijay Chaure <vijaychaure7494@gmail.com>
2024-08-21 21:22:25 +0530

    Main Page after login changed to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Dashboard from "./dashboard/page";
+import SideNav from "./dashboard/_components/SideNav";
 
 export default function Home() {
   return (
+  <>
+    <SideNav/>
     <Dashboard/>
+  </>
   );
 }
