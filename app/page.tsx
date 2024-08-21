54dd47437fd354b09eb0e995bfd031f6f256395f
Vijay Chaure <vijaychaure7494@gmail.com>
2024-08-21 21:20:34 +0530

    Main Page after login changed to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
-    <main>
-      <div className="">
-
-      </div>
-      <div className="">
-        <Button>hello sir</Button>
-      </div>
-    </main>
+    <Dashboard/>
   );
 }
